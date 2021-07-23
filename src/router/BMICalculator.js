7f5e869484214733f069b2b42db82e44432fa09b
Gautam Saini <gautamsaini455@gmail.com>
2021-07-23 20:35:04 +0530

    Updated the route file.

--- src/router/BMICalculator.js
+++ src/router/BMICalculator.js
@@ -3,29 +3,39 @@ const BMIController = require("../controller/BMIController");
 
 const router = new express.Router();
 
-// BMI calculator
+// BMI calculator API
 router.post("/bmi", (req, res) => {
   try {
     let data = req.body,
       count = 0;
     const result = {};
+    // Calling CalculateBMI function.
     data = BMIController.calculateBMI(data);
+
+    // Calling CategoryAndHealthRisk function.
     data = BMIController.CategoryAndHealthRisk(data);
+
+    //Calling countOverweight Function.
     count = BMIController.countOverweight(data);
+
+    // Gaurd clause for checking data.
     if (!data) {
       throw new Error("No Data found!");
     }
 
+    //Assigning data to result object.
     result.data = data;
     result.countOverWeight = count;
     // console.log("Data:", result);
 
+    // Send response with 200 status code.
     res.status(200).send({
       success: "true",
       status: 200,
       data: result,
     });
   } catch (e) {
+    // Sending response of error.
     res.status(400).send({
       success: "false",
       status: 400,
